e332bf804d9acbae83090a2c9516e922b87ee481
Dalton <slattery.da@gmail.com>
2021-12-14 11:43:38 -0600

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+//create h2 element and add text content to it
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+//adding h2 element to the body
+document.querySelector("body").appendChild(h2);
